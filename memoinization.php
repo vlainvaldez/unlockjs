24533e26dcdfbcc5867a72b706b3daabee1681ef
Alvin Valdez <alvinjosephvaldez@yahoo.com>
2014-04-16 13:24:13 +0800

    page 112 in javascript ninja

--- memoinization.php
+++ memoinization.php
@@ -24,29 +24,53 @@
 
 
 <script type="text/javascript">
-    Function.prototype.memoized = function(key){
-        this.alvin = this.alvin || {}; 
-        return this.alvin[key] !== undefined ? 
-        this.alvin[key] :
-        this.alvin[key] = this.apply(this, arguments);
-        
-        };
+    // Function.prototype.memoized = function(key){
+    //     this.alvin = this.alvin || {}; 
+    //     return this.alvin[key] !== undefined ? 
+    //     this.alvin[key] :
+    //     this.alvin[key] = this.apply(this, arguments);
+    //     };
 
-    function isPrime( num ) { 
-        var prime = num != 1;
-        for ( var i = 2; i < num; i++ ) {
-            if ( num % i == 0 ) {
-            prime = false;
-            break;
-            }
-        }
-        return prime;
+    // Function.prototype.memorize = function() {
+    //     var fn = this;
+    //     return function() {
+    //         return fn.memoized.apply(fn, arguments);            
+    //     }
+    // };
+
+
+    // var isPrime = (function (num){
+    //     var prime = num != 1;
+    //     for ( var i = 2; i < num; i++ ) {
+    //         if ( num % i == 0 ) {
+    //         prime = false;
+    //         break;
+    //         }
+    //     }
+    //     return prime;
+    // }).memorize();
+
+    // console.log(isPrime(5), 
+    // "The function works; 5 is prime."); 
+    
+    // console.log(isPrime.alvin[5], 
+    // "The answer has been cached."); 
+</script>
+<script type="text/javascript">
+    function wrap(object, method, wrapper) {
+        var fn = object[method]; return object[method] = function() {
+        return wrapper.apply(this, [ fn.bind(this) ].concat(
+        Array.prototype.slice.call(arguments))); } };
+
+
+    // Example adapted from Prototype
+    if (Prototype.Browser.Opera) {
+    wrap(Element.Methods, "readAttribute", function(original, elem, attr) {
+           return attr == "title" ?
+               elem.title :
+               original(elem, attr);
+       });
     }
 
-    console.log(isPrime.memoized(5), 
-    "The function works; 5 is prime."); 
-    
-    console.log(isPrime.alvin[5], 
-    "The answer has been cached."); 
 </script>
 </html>
