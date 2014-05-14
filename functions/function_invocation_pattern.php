12a4e9a7290bb48b1d865c648ee1aaaf23896f4a
alvin <alvinjosephvaldez@yahoo.com>
2014-05-14 17:26:37 +0800

    understanding this

--- functions/function_invocation_pattern.php
+++ functions/function_invocation_pattern.php
@@ -30,6 +30,25 @@ var add = function(a, b){
 
 var sum = add(2,3); //5
 
+var value = 500;
+
+var obj = {
+    value: 0,
+    increment: function (){
+        this.value++;
+
+        var that = this;
+
+        var innerFunction = function (){
+            alert(this.value); // this.value became global because of innerFunction(); bellow the global this.value is var = 500 
+        };
+        
+        innerFunction();
+    },
+};
+
+obj.increment();
+
 
 </script>
 </html>
