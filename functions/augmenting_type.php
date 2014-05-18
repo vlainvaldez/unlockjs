40b0fda1d71a8aacd474b66f7fc94a677aad0df2
alvin <alvinjosephvaldez@yahoo.com>
2014-05-18 23:21:24 +0800

    adding comments

--- functions/augmenting_type.php
+++ functions/augmenting_type.php
@@ -11,7 +11,8 @@
 </body>
 
 <script type="text/javascript">
-
+// we use this to shorten the use of
+// Obj.prototype.obj 
 Function.prototype.protos = function (name, func){
     if(!this.prototype[name]){
         this.prototype[name] = func;
@@ -23,6 +24,12 @@ var Car = function (model){
     this.model = model
 };
 
+// this is how we use it.
+// the first parameter is the name of the prototype
+// the second is the function
+
+// so if we use the old one it would be like this
+// Car.prototype.get_model = function() {};
 Car.protos('get_model', function(){
     return this.model;
 });
@@ -31,10 +38,16 @@ var teslaCar = {
     model: "tesla",
 };
 
+// with the use of apply invocation inside the augmented prototype
+// instead of making this " var tesla = Car.prototype.get_model.apply(teslaCar); "
+// from the old one we can use the new one below
+// we use "this" in this.apply()
+// because the this == get_model because its inside the context of "get_model"
 var tesla = Car.protos('get_model', function(){
     this.apply(teslaCar);
 });
 
+// we still have the same result as the apply_invocation_patern_v2.php
 
 alert(tesla);
 
