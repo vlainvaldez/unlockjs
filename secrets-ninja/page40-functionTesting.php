14c21f2f94a34aa5cf1dc5e1c94091dc63b9a422
alvin joseph valdez <vlainlawliet@gmail.com>
2014-09-22 11:04:49 +0800

    fix the indention

--- secrets-ninja/page40-functionTesting.php
+++ secrets-ninja/page40-functionTesting.php
@@ -9,38 +9,30 @@
 </body>
 <script type="text/javascript" src="assert.js"></script>
 <script type="text/javascript">
- function isNimble(){ return true; } //#1
+ function isNimble(){ return true; } 
 
-assert(typeof window.isNimble === 'function', //#2
-"isNimble() defined");
+assert(typeof window.isNimble === 'function', "isNimble() defined");
 
-assert(isNimble.name === 'isNimble',
-"isNimble() has a name");
+assert(isNimble.name === 'isNimble', "isNimble() has a name");
 
-var canFly = function(){ return true; }; //#3
-assert(typeof window.canFly === 'function', //#4
-"canFly() defined");
+var canFly = function(){ return true; }; 
+assert(typeof window.canFly === 'function', "canFly() defined");
 
-assert(canFly.name === '',
-"canFly() has no name");
+assert(canFly.name === '', "canFly() has no name");
 
-window.isDeadly = function(){ return true; }; //#5
-assert(typeof window.isDeadly === 'function', //#6
-"isDeadly() defined");
+window.isDeadly = function(){ return true; }; 
+assert(typeof window.isDeadly === 'function', "isDeadly() defined");
 
-function outer(){ //#7
-	assert(typeof inner === 'function',
-	"inner() in scope before declaration");
+function outer(){ 
+	assert(typeof inner === 'function',"inner() in scope before declaration");
 
 	function inner(){}
-	assert(typeof inner === 'function',"inner() in scope after declaration");
-	assert(window.inner === undefined,
-	"inner() not in global scope");
+	assert(typeof inner === 'function', "inner() in scope after declaration");
+	assert(window.inner === undefined, "inner() not in global scope");
 };
 
-outer(); //#8
-assert(inner === undefined,
-"inner() still not in global scope");
+outer(); 
+assert(inner === undefined, "inner() still not in global scope");
 
 </script>
 </html>
